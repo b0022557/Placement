a9a6481166677d0ba30864799c4aebb1e9c219f7
Your Name <b0022557@my.shu.ac.uk>
2012-05-05 23:46:36 +0100

    Bootstrap Class Status Codes

--- Placement/grails-app/conf/BootStrap.groovy
+++ Placement/grails-app/conf/BootStrap.groovy
@@ -1,4 +1,6 @@
 import placement.Status
+import placement.Student
+import placement.Placement_opportunity
 
 class BootStrap {
 
@@ -11,6 +13,18 @@ class BootStrap {
         def acceptsta = new Status(code:'Accepted', description:'Student has accepted a placement').save();
         def rejectsta = new Status(code:'Rejected', description:'Student has rejected a placement').save();
 
+
+        def johnstu = new Student(applications:'1', course_code:'3456',name:'John Smith', notes:'Job within home living area ').save();
+        def hannahstu = new Student(applications:'4', course_code:'3091',name:'Hannah Proctor', notes:'').save();
+        def amystu = new Student(applications:'2', course_code:'3091',name:'Amy Harris', notes:'Job within home living area ').save();
+        def matthewstu = new Student(applications:'3', course_code:'3091',name:'Matthew Jackson', notes:' ').save();
+        def stevestu = new Student(applications:'1', course_code:'3456',name:'Steve Jones', notes:' ').save();
+
+        def cyber1place = new Placement_opportunity(applications:'3',company_name:'Cyber Computers', job_title:'Support Analyst',status:'Open').save();
+        def JTtechplace = new Placement_opportunity(applications:'1',company_name:'JT Technology', job_title:'Technician',status:'Open').save();
+        def cyber2place = new Placement_opportunity(applications:'10',company_name:'Cyber Computers', job_title:'Database Administrator',status:'Closed').save();
+        def capitalsolplace = new Placement_opportunity(applications:'5',company_name:'Capital Solutions', job_title:'Software Engineer',status:'Open').save();
+        def eazytechplace = new Placement_opportunity(applications:'8',company_name:'Eazy Tech', job_title:'Technician',status:'Closed').save();
 
     }
     def destroy = {
